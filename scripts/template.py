734d0692ac4151e0c6037aa92c0d5731f67053fe
thanhdc <thanhdc@ql6625.fr>
2020-07-18 12:00:06 +0700

    Update template: get rid of params

--- scripts/template.py
+++ scripts/template.py
@@ -2,73 +2,69 @@ import numpy as np
 from PIL import Image, ImageFont, ImageDraw
 from common import glyph_matrix
 
-def draw_letter(draw, dx, dy, letter, font, params):
+def draw_grid(draw, dx, dy, *, line_width, em, ascent, capHeight, xHeight):
 
   def horz(x, y, color):
-    draw.line((x, y, x + params['em_width'], y), fill=color, width=params['line_width'])
+    xy = np.array((x, y, x + em, y))
+    draw.line(tuple(xy), fill=color, width=line_width)
 
   def vert(x, y, color):
-    draw.line((x, y, x, y + params['em']), fill=color, width=params['line_width'])
+    xy = np.array((x, y, x, y + em))
+    draw.line(tuple(xy), fill=color, width=line_width)
 
-  # Gray box: top (ascent), bottom (descent), left, right
-  horz(dx, dy, 'lightgray')
-  horz(dx, dy + params['em'], 'lightgray')
+  # Gray box: left, right, top (ascent), bottom (descent)
   vert(dx, dy, 'lightgray')
-  vert(dx + params['em_width'], dy, 'lightgray')
-  
+  vert(dx + em, dy, 'lightgray')
+  horz(dx, dy, 'gray')
+  horz(dx, dy + em, 'gray')
+
   # baseline, cap height, x-height
-  horz(dx, dy + params['ascent'], 'red')
-  horz(dx, dy + params['ascent'] - params['caps'], 'lightgreen')
-  horz(dx, dy + params['ascent'] - params['x_height'], 'lightgray')
-
-  # width, height = draw.textsize(letter, font=params['font'])
-  width, height = letter.width, letter.vwidth
-  draw.text((dx + (params['em_width'] - width) / 2, dy), chr(letter.unicode), font=font, fill=params['text_color'])
+  horz(dx, dy + ascent, 'red')
+  horz(dx, dy + ascent - xHeight, 'lightblue')
+  horz(dx, dy + ascent - capHeight, 'lightgreen')
 
 
-def make_template(sample, matrix, padding, params):
-  px, py = padding
-  rows, columns = np.array(matrix).shape
+def make_template(sample, size, scale):
+  rows, columns = size
 
-  img = Image.new('RGB', (columns * (px + params['em_width']), rows * (py + params['em'])), 'white')
+  font = fontforge.open(sample)
+  draw_font = ImageFont.truetype(sample, int(font.em * scale))
+  matrix = glyph_matrix(letters=letters, font=font, rows=columns, columns=columns)
+  
+  ascent, descent = draw_font.getmetrics()
+  em = ascent + descent
+  xHeight = descent * (font.xHeight / font.descent)
+  capHeight = descent * (font.capHeight / font.descent)
+
+  img = Image.new('RGB', (em * columns, em * rows), 'white')
   draw = ImageDraw.Draw(img)
-  font = ImageFont.truetype(sample, params['em'])
 
   for r, row in enumerate(matrix):
-    for c, ch in enumerate(row):
-      # Top-left corner
-      dx = c * (px + params['em_width'])
-      dy = r * (py + params['em'])
-      draw_letter(draw, dx, dy, ch, font, params)
+    for c, letter in enumerate(row):
+      dx = em * c
+      dy = em * r
+      # Draw grid
+      draw_grid(draw, dx, dy, line_width=3, em=em, ascent=ascent, capHeight=capHeight, xHeight=xHeight)
+
+      text = chr(letter.unicode)
+      x = dx + (em - draw_font.getsize(text)[0]) / 2
+      y = dy
+      draw.text((x, y), text, font=draw_font, fill='lightgray')
   
   return img
 
 
-params = {
-  'em': 1000,
-  'em_width': 1000,
-  'x_height': 368,
-  'caps': 650,
-  'ascent': 800,
-  #descent
-
-  # Draw props
-  'font_size': 1000, # = em
-  'line_width': 3,
-  'text_color': 'lightgray'
-}
-
-
-def gen(input, output, letters, size):
+def gen(input, output, letters, size, scale):
   rows, columns = size
-  matrix = glyph_matrix(letters=letters, font=fontforge.open(input), rows=columns, columns=columns)
-  img = make_template(input, matrix, (0, 0), params)
-  ratio = 2
-  width, height = img.size
-  img.thumbnail((width / ratio, height / ratio))
+  img = make_template(input, size, scale)
   img.save(output)
+
 
 # letters = 'HOnodpagscebhklftijmnruwvxyzCGABRDLEFIJKMNPQSTUVWXYZ0123456789?:;-–—=!\'’"“”@/\\~_#$%&()*+,.<>[]^`{|}q'
 letters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ?:;-–—=!\'’"“”@/\\~_#$%&()*+,.<>[]^`{|}'
-gen('assets/sachacHand.otf', 'template-sachacHand.png', letters, (10, 10))
+input = 'roboto/Roboto-Regular.ttf'
+output = 'Roboto-template.png'
+# input = 'assets/sachacHand.otf'
+# output = 'sachacHand_template.png'
+gen(input, output, letters, size=(10, 10), scale=.5)
 print("Success")
